d4d15b7792bd5042faaa0a4ed07d516e8769a55f
filedownloade <my1005ali@gmail.com>
2024-12-18 12:39:04 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function namecal() {
     if (document.getElementById("name").value === "10201020") {
-        alert("downloading...")
+        alert("downloading...Farsi")
 
         var link = document.createElement('a');
-        link.innerText = "Riazi"
-        link.href = 'Riazi-q.pdf'; 
-        link.download = 'Riazi-q.pdf'; 
+        link.innerText = "Farsi"
+        link.href = 'new_farsi.pdf'; 
+        link.download = 'new_farsi.pdf'; 
 
 
         document.body.appendChild(link);
